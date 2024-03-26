238a40837f88842f379cafa4b46a0bbeb8b2c789
Yardley Francois <144744560+YardleyFra@users.noreply.github.com>
2024-03-26 08:33:24 -0400

    server

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ app.use(express.static('public'));
 
 // Set the root route to serve 'index.html'
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
